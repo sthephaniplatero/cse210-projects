5fca32af2f5ae4b94f7f150e9a604f3394bb7cdd
sthephaniplatero <pla21002@byui.edu>
2024-09-16 15:17:59 -0600

    Practice variables, input and output.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What's your firstname? ");
+        string name =Console.ReadLine();
+        Console.Write("What's your lastname? ");
+        string lastname =Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {name} {lastname}.");
+
     }
 }
