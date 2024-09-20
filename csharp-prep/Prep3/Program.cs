4795de8515a483dc50dfb7b6e2237807d9d50f87
sthephaniplatero <pla21002@byui.edu>
2024-09-20 12:31:16 -0600

    New project

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,44 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   // Add ramdon generator
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        int guess = -1;
+
+        while (guess != magicNumber )
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+             if (guess > magicNumber)
+
+        {
+           Console.WriteLine("Lower"); 
+        }
+
+        else if (guess < magicNumber)
+
+        {
+           Console.WriteLine("Higher"); 
+        }
+
+        else
+        {
+            Console.WriteLine("You guess it!"); 
+           
+        }
+
+
+        
+
+        }
+
+
+       
+
+
+       
+
     }
 }
