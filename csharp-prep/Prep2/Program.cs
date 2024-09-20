1631d3f5ba3f1ee9b4c5d3dbf367a535bcbbc5c6
sthephaniplatero <pla21002@byui.edu>
2024-09-20 12:39:24 -0600

    Add comments to the project.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,14 +3,15 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    {   // Ask the user for the percentage
         Console.Write("Please type your grade percentage: ");
         string valueFromUser = Console.ReadLine();
-
         int grade = int.Parse(valueFromUser);
-        
-        string letter = "";
 
+        //Create the variable letter to be use in the staments 
+        string letter = "";
+        
+        
         if (grade >= 90)
         {
             letter = "A";
@@ -36,9 +37,10 @@ class Program
             letter = "F";
         
         }
-
+        // Print the grades
         Console.WriteLine($"Your grade is : {letter}");
-
+        
+        // Verify is the student pass the curse with 70
         if (grade >= 70)
          {
             Console.WriteLine("Congratulation you pass the course");
