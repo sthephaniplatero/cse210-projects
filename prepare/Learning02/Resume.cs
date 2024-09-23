1faec0f170335f9f66d209533ac73990f597f222
sthephaniplatero <pla21002@byui.edu>
2024-09-23 17:46:00 -0600

    Week 2

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Resume 
+{
+    public string _personName;
+    public List<Job>_jobs = new List<Job>();
+
+    public void DisplayResult()
+    {
+        Console.WriteLine($"Name: {_personName}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+
+        
+
+    }
+
+
+
+
+}
