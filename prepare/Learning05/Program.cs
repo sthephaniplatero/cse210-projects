4dbb7488f15e27781678b90c159bd8871d4e1635
sthephaniplatero <pla21002@byui.edu>
2024-10-16 09:56:11 -0600

    Assignment Program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment1 = new Assignment("Samuel Benett","Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+
+        WritingAssignment assignment3 = new WritingAssignment("Mary Waters","European History","The causes of the World War II");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWrittingInformation());
+
+
     }
 }
